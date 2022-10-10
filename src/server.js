d49b176704fd054d3e23b65d0888100cc76c4be2
Mayank <45217514+mayankdp@users.noreply.github.com>
2022-10-10 00:46:54 -0400

    Dyanmic Website - Assignment

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 // use the express library
 const express = require('express');
+// use the cookieParser library
+const cookieParser = require('cookie-parser');
 
 // create a new server application
 const app = express();
@@ -10,11 +12,25 @@ const app = express();
 // world wide web).
 const port = process.env.PORT || 3000;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(cookieParser());
 
-// The main page of our website
+let nextVisitorId = 1;
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.cookie('visitorId', nextVisitorId);
+  res.cookie('visited', Date.now().toString());
+  res.render('welcome', {
+    name: req.query.name || "World",
+    date: new Date().toLocaleString(),
+    seconds: Math.ceil((Date.now().toString() - req.cookies.visited) / 1000),
+    id: req.cookies['visitorId'] != null ? req.cookies['visitorId'] : nextVisitorId++,
+  });
+  console.log(req);
 });
+
+app.use(express.static('public'));
 
 // Start listening for network connections
 app.listen(port);
