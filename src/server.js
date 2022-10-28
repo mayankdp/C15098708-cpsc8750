53f526a61a731d933ef0b171f75c83c419cf9346
Mayank <45217514+mayankdp@users.noreply.github.com>
2022-10-28 15:24:40 -0400

    RESTful Gateway part

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 // use the express library
 const express = require('express');
+
+// Server Implementation (Trivia API)
+const fetch = require('node-fetch');
+
 // use the cookieParser library
 const cookieParser = require('cookie-parser');
 
@@ -28,6 +32,81 @@ app.get('/', (req, res) => {
     id: req.cookies['visitorId'] != null ? req.cookies['visitorId'] : nextVisitorId++,
   });
   console.log(req);
+});
+
+let nextQuestion = 0;
+let options =[];
+
+app.get("/trivia", async (req, res) => {
+  // fetch the data
+  const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
+
+  // fail if bad response
+  if (!response.ok) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+    return;
+  }
+
+  // interpret the body as json
+  const content = await response.json();
+
+  // fail if db failed
+
+  if (content.response_code !== 0) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with internal response code ${content.response_code}`);
+    return;
+  }
+
+  // respond to the browser
+  // TODO: make proper html
+  // res.send(JSON.stringify(content, 2));
+
+  const randomNum = Math.floor(Math.random() * 4);
+  const correctAnswer = content.results[0].correct_answer;
+  const otherOption1 = content.results[0].incorrect_answers[0];
+  const otherOption2 = content.results[0].incorrect_answers[1];
+  const otherOption3 = content.results[0].incorrect_answers[2];
+  if (randomNum == 0) {
+    options[0] = correctAnswer;
+    options[1] = otherOption1;
+    options[2] = otherOption2;
+    options[3] = otherOption3;
+  } else if (randomNum == 1){
+    options[1] = otherOption1;
+    options[0] = correctAnswer;
+    options[2] = otherOption2;
+    options[3] = otherOption3;
+  } else if (randomNum == 2){
+    options[2] = otherOption2;
+    options[1] = otherOption1;
+    options[0] = correctAnswer;
+    options[3] = otherOption3;
+  } else if (randomNum == 3){
+    options[3] = otherOption3;
+    options[2] = otherOption2;
+    options[1] = otherOption1;
+    options[0] = correctAnswer;
+  }
+  // console.log(randomNum);
+  // console.log(content.results[0]);
+
+  res.render('trivia', {
+    question: content.results[0].question,
+    answers: options,
+    category: content.results[0].category,
+    difficulty: content.results[0].difficulty,
+  });
+
+  const answerLinks = options.map(answer => {
+    return `<a href="javascript:alert('${
+      answer === correctAnswer ? 'Correct!' : 'Incorrect, Please Try Again!'
+      }')">${answer}</a>
+    }`
+  })
+
+  nextQuestion++;
 });
 
 app.use(express.static('public'));
